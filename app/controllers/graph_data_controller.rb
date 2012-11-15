f4a7936ec9f71c37fdecfdf8b62074fc12f62e43
Takaaki Muta <muta.takaaki@hcc-jp.com>
2012-11-15 19:15:22 +0900

    調整

--- app/controllers/graph_data_controller.rb
+++ app/controllers/graph_data_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class GraphDataController < ApplicationController
 
   class Graph
@@ -5,7 +6,42 @@ class GraphDataController < ApplicationController
     end
 
     def to_json
-      {chart: {renderTo: 'graph'}}
+      {
+        chart: {
+          renderTo: 'graph'
+        },
+        title: {
+          text: "Average Monthly Weather Data for Tokyo"
+        },
+        xAxis: [
+          { categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+        ],
+        yAxis: [
+           {
+             title: {
+               text: nil,
+               style: {
+                 color: "#4572A7"
+               }
+             },
+             labels: {
+               style: {
+                 color: "#4572A7"
+               }
+             }
+           }
+         ],
+        tooltip: { enabled: false },
+        legend: { enabled: false },
+        series: [
+          {
+            name: nil,
+            color: "#4572A7",
+            type: "column",
+            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+          }
+        ]
+      }
     end
   end
 
